cb34e9512fad5db68baafea25707c0d5a374dd31
Mitchell DeCarlo <decarlomitch9@gmail.com>
2022-05-11 18:05:37 -0400

    Second commit added child process zombie

--- z_creator.c
+++ z_creator.c
@@ -4,5 +4,15 @@
   
 int main()
 {
-    int zom = fork();
+    int proc_pid = fork();
+    
+    if (proc_pid > 0){
+        printf("In parent process!\n");
+    }
+    else if (proc_pid == 0){
+        printf("In child process");
+        sleep(5);
+    }
+
+    return 0;
 }
