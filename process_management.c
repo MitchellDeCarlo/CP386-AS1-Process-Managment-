eed787ec7704dc765a9367ff615b22709432ada5
Mitchell DeCarlo <decarlomitch9@gmail.com>
2022-05-20 15:17:08 -0400

    Started Q3

---
+++ process_management.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <sys/wait.h>
+
+int main(int argc,char* argv[])
+{
+    printf("Hello world\n");
+
+    return 0;
+}
