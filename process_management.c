376735fea11a57aef5de44a8f6bb5f305ac3e60a
Mitchell DeCarlo <decarlomitch9@gmail.com>
2022-05-21 16:05:02 -0400

    Added memory buffer for Q3

--- process_management.c
+++ process_management.c
@@ -5,9 +5,17 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
 
 int main(int argc,char* argv[])
 {
+    void *ptr;
+    int shm_fd;
+    const int SIZE = 4096;
+    const char *name = "OS"; 
     pid_t child1 = fork();
     char comm[100];
     if(child1 == 0){
@@ -22,11 +30,25 @@ int main(int argc,char* argv[])
                 comm[index++] = ch;
             }
         }
-        printf("%s",comm);
         fclose(fp);
+
+        shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+        ftruncate(shm_fd,SIZE);
+        ptr = mmap(0,SIZE,PROT_WRITE,MAP_SHARED,shm_fd,0);
+
+        sprintf(ptr,"%s",comm);
+        ptr += strlen(comm);
     }
     else{
         wait(NULL);
+        
+        shm_fd = shm_open(name, O_RDONLY, 0666);
+
+        ptr = mmap(0,SIZE,PROT_READ,MAP_SHARED,shm_fd,0);
+
+        printf("%s\n",(char *)ptr);
+
+        shm_unlink(name);
         
     }
 
