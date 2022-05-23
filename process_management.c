226eec236052212f1c1d1fe871c1b97363ecc573
Mitchell DeCarlo <decarlomitch9@gmail.com>
2022-05-22 23:04:26 -0400

    Added code for second child process

--- process_management.c
+++ process_management.c
@@ -10,46 +10,85 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 
-int main(int argc,char* argv[])
+int main(int argc, char *argv[])
 {
     void *ptr;
     int shm_fd;
     const int SIZE = 4096;
-    const char *name = "OS"; 
+    const char *name = "OS";
     pid_t child1 = fork();
     char comm[100];
-    if(child1 == 0){
-        FILE *fp = fopen(argv[1],"r");
+    if (child1 == 0)
+    {
+        FILE *fp = fopen(argv[1], "r");
         char ch;
         int index = 0;
-        if (fp == NULL){
+        if (fp == NULL)
+        {
             printf("Error opening file.\n");
         }
-        else{
-            while ((ch = fgetc(fp)) != EOF){
+        else
+        {
+            while ((ch = fgetc(fp)) != EOF)
+            {
                 comm[index++] = ch;
             }
         }
         fclose(fp);
 
         shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
-        ftruncate(shm_fd,SIZE);
-        ptr = mmap(0,SIZE,PROT_WRITE,MAP_SHARED,shm_fd,0);
-
-        sprintf(ptr,"%s",comm);
+        ftruncate(shm_fd, SIZE);
+        ptr = mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
+        sprintf(ptr, "%s", comm);
         ptr += strlen(comm);
     }
-    else{
+    else
+    {
         wait(NULL);
-        
-        shm_fd = shm_open(name, O_RDONLY, 0666);
+        pid_t child2 = fork();
 
-        ptr = mmap(0,SIZE,PROT_READ,MAP_SHARED,shm_fd,0);
+        if (child2 == 0)
+        {
+            shm_fd = shm_open(name, O_RDONLY, 0666);
+            ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
+            char commands[40];
+            char temp[20];
+            strcpy(commands, (char *)ptr);
 
-        printf("%s\n",(char *)ptr);
+            char **arr = malloc(5 * sizeof(char*));
 
-        shm_unlink(name);
-        
+            for (int i =0; i < 5; i++){
+                arr[i] = (char *)malloc(30 * sizeof(char));
+            }
+            int iter = 0;
+            int iter2 = 0;
+            for (int i = 0; i < strlen(commands); i++)
+            {
+                if (commands[i] != '\n')
+                {
+                    
+                    temp[iter++] = commands[i];
+                }
+                else
+                {
+                   // printf("%s\n",temp);
+                    arr[iter2++] = temp;
+                    for (int i = 0; i < 20; i++)
+                    {
+                        temp[i] = '\0';
+                    }
+                    iter =0;
+                }
+            }
+
+            for(int i =0; i < 5; i++){
+               printf("%s\n",arr[i]);
+            }
+
+            shm_unlink(name);
+
+            free(arr);
+        }
     }
 
     return 0;
